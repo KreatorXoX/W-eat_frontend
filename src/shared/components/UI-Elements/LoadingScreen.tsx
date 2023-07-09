d7b8fb626d828afddc1ba72e5a8941142ee80443
Gorkem Gocer <gorkemgocer7@gmail.com>
2023-07-09 13:25:44 +0300

    Loading screen modified and made better

--- src/shared/components/UI-Elements/LoadingScreen.tsx
+++ src/shared/components/UI-Elements/LoadingScreen.tsx
@@ -1,18 +1,35 @@
 import { GiMonoWheelRobot } from "react-icons/gi";
+import { createPortal } from "react-dom";
 type Props = {};
 const LoadingScreen = (props: Props) => {
-  return (
-    <div className="relative h-screen bg-gray-400/60">
-      <div className="absolute top-[35%] left-[40%] rounded-full bg-gray-800/70 p-10 text-7xl ">
-        <div className="flex flex-col items-center justify-center">
-          <h2 className="absolute -top-2 text-4xl text-slate-200">W-eat</h2>
-          <GiMonoWheelRobot className="animate-wiggle text-red-500" />
-          <h2 className="absolute -top-20 ml-[14px] text-5xl italic tracking-[14px] text-gray-800">
-            Loading
-          </h2>
+  const content = (
+    <div className="absolute top-0 z-20 h-full w-full">
+      <div className="flex h-screen flex-col items-center justify-center space-y-5 bg-slate-100">
+        <div className="text-2xl md:text-4xl">
+          <span className="text-red-400">w</span>/
+          <span className="text-blue-400">EAT</span>
         </div>
+        <div className="rounded-full bg-red-400 p-2">
+          <div className="rounded-full bg-yellow-400 p-2">
+            <div className="rounded-full bg-green-400 p-2">
+              <div className="rounded-full bg-blue-400 p-2">
+                <div className="h-24 w-24 rounded-full bg-slate-50 p-3 ">
+                  <GiMonoWheelRobot className="h-full w-full animate-wiggle text-purple-600" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h2 className="ml-[14px] text-xl font-medium italic tracking-[14px] text-blue-800 md:text-3xl">
+          Loading ...
+        </h2>
       </div>
     </div>
+  );
+
+  return createPortal(
+    content,
+    document.getElementById("loadingScreen") as HTMLElement
   );
 };
 
